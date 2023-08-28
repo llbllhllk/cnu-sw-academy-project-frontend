a17e24bffb1431376fd1e7e58570501e47035a34
YongJin <0jin.git@gmail.com>
2023-08-29 03:49:36 +0900

    Feat: Home page 게시판 스타일링

--- src/components/board/HotBoard.js
+++ src/components/board/HotBoard.js
@@ -6,12 +6,12 @@ import Button from 'components/common/Button'
 const HotBoard = () => {
   return (
     <Card title="Hot 게시판">
-        {/* <button>더보기</button> */}
+        <button style={{position:'absolute',top:'35px', right:'20px'}}>더보기</button>
         <BoardItem />
         <BoardItem />
         <BoardItem />
         <BoardItem /> 
-      {/* 최대 4개 */}
+      {/* 최대 4개까지 넣어유 */}
 
       <div style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
         <Button width={'60px'} height={'50px'}>글쓰기</Button>
