b6186d74adba44edac85919b76ab4167962895fa
kimhyeonju <hyeonju0014@naver.com>
2023-08-31 19:13:42 +0900

    Feat: 식당 상세보기 기능 추가

---
+++ src/components/storeBoard/storeInfo/ReviewProfileBox.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+const ReviewProfileBox = ({width, height, top, children,...props}) => {
+  return (
+    <ReviewInfoBoxStyle width={width} height={height} top={top} {...props}>
+    {children}
+    </ReviewInfoBoxStyle>
+  );
+};
+
+const ReviewInfoBoxStyle = styled.div`
+  width: ${({ width }) => (width ? `${width}` : `100%`)};
+  height: ${({ height }) => (height ? `${height}` : `auto`)};
+  margin-top: ${({ top }) => (top ? `${top}` : `0px`)};
+  border: 2px solid #d9d9d9;
+  border-radius: 15px;
+  background-color: #f5f5f5;
+
+`;
+
+
+export default ReviewProfileBox;
---
+++ src/components/storeBoard/storeInfo/index.js
@@ -0,0 +1,124 @@
+import Button from 'components/common/Button';
+import Card from 'components/common/Card';
+import StoreInfoBox from './StoreInfoBox';
+import ReviewProfileBox from './ReviewProfileBox';
+import CircularProfile from './CircularProfile';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import PhoneIcon from '@mui/icons-material/Phone';
+// import FaceIcon from '@mui/icons-material/Face';
+import StarIcon from '@mui/icons-material/Star';
+import styled from 'styled-components';
+
+const StoreInfo = ({ title, children}) => {
+    return (
+        <>
+        <ButtonBox>
+        <Button width='72px' height='30px'>게시판</Button>
+        <Spacer/>
+        <Button width='120px' height='30px'>식당 상세 정보</Button>
+        </ButtonBox>
+
+        <Card title={title} width='50%'>
+
+        <div height='100px' style={{ display: 'flex', justifyContent: 'space-between'}}>
+            <StoreInfoBox width='46%' height='130px' top='30px'>
+                <div style={{ display: 'flex', flexDirection: 'column', marginLeft: '10px'}}>
+                    <IconBox><Star/><InfoTextBox>4.25 / 5.0</InfoTextBox></IconBox>
+                    <IconBox><LocationOnIcon/><InfoTextBox>대전시 유성구 궁동 000-00</InfoTextBox></IconBox>
+                    <IconBox><PhoneIcon/><InfoTextBox>042-000-0000</InfoTextBox></IconBox>
+                </div>
+            </StoreInfoBox>
+            <StoreInfoBox width='46%' height='160px'>지도</StoreInfoBox>  
+        </div>
+
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginLeft: '10px', marginTop: '20px', marginBottom: '20px'}}>
+        <ReviewTextBox>Review</ReviewTextBox>
+        <Button width='75px' height='30px'>리뷰 작성</Button>
+        </div>
+        
+        <StoreInfoBox width='100%' height='180px'>
+        <CenteredBox>
+          <ReviewProfileBox width='95%' height='75px' top='7px'>
+            <ProfileContainer>
+              <CircularProfile></CircularProfile>
+              <ProfileInfo>
+                <div>익명 1</div>
+                <WriteBox></WriteBox>
+              </ProfileInfo>
+            </ProfileContainer>
+          </ReviewProfileBox>
+          <ReviewProfileBox width='95%' height='75px' top='10px'>
+          <ProfileContainer>
+              <CircularProfile></CircularProfile>
+              <ProfileInfo>
+                <div>익명 2</div>
+                <WriteBox></WriteBox>
+              </ProfileInfo>
+            </ProfileContainer>
+          </ReviewProfileBox>
+        </CenteredBox>
+      </StoreInfoBox>
+      </Card>
+      </>
+        
+    );
+};
+
+const ButtonBox = styled.form`
+  margin-bottom: 10px;
+  display: flex;
+`;
+
+const Spacer = styled.div`
+  width: 15px; 
+`;
+
+const ReviewTextBox = styled.div`
+
+  font-size: 18px;
+  font-weight: 700;
+`;
+
+const IconBox = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 12px;
+`;
+
+const InfoTextBox = styled.div`
+  margin-left: 10px;
+`;
+
+const CenteredBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+const Star = styled(StarIcon)`
+  color: #FC4C4E;
+`;
+
+const WriteBox = styled.div`
+  border: 2px solid #D9D9D9;
+  width: 500px;
+  height: 40px;
+  border-radius: 10px;
+  background-color: #D9D9D9;
+  position: relative; 
+  z-index: 2;
+
+`;
+
+const ProfileContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const ProfileInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 20px;
+`;
+
+export default StoreInfo;
