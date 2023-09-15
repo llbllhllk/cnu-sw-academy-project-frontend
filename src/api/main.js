72f38592a6dc28a8b8cffc890edd2ec21e48a0a4
YongJin <0jin.git@gmail.com>
2023-09-15 15:57:30 +0900

    f

--- src/api/main.js
+++ src/api/main.js
@@ -1,5 +1,5 @@
 import instance from 'api';
-import axios from 'axios';
+
 
 export const fetchGetFreeBoard = async () => {
   return (await instance.get('/main/freeboard')).data;
@@ -13,6 +13,3 @@ export const fetchGetSlider = async () => {
   return (await instance.get('/main/recent')).data;
 };
 
-export const fetchGetStoreList = async() => {
-  return (await axios.get('https://jsonplaceholder.typicode.com/posts')).data;
-}
