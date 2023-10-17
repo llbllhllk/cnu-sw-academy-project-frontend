76a4e257e6d3faa61530db5b52de61ac22284300
LEE YONGJIN <0jin.git@gmail.com>
2023-10-17 15:57:20 +0900

    Feat: 자유게시판 만드는 중

--- src/route/index.js
+++ src/route/index.js
@@ -3,6 +3,8 @@ import Store from 'pages/Store';
 import Filter from 'pages/Filter';
 import Login from 'pages/Login';
 import SignUp from 'components/login/SignUp';
+import Popular from 'pages/Popular';
+import Hot from 'pages/Hot';
 import { Route, Routes } from 'react-router-dom';
 
 const Router = () => {
@@ -13,6 +15,8 @@ const Router = () => {
       <Route path='/filter' element={<Filter/>} />
       <Route path='/login' element={<Login/>} />
       <Route path='/signup' element={<SignUp/>} /> 
+      <Route path='/popular' element={<Popular/>} /> 
+      <Route path='/hot' element={<Hot/>} /> 
       {/* 회원가입 */}
 
     </Routes>
