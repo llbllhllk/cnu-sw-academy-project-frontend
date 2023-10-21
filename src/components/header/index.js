3fddec184b9ed73ddc823e83bd4e43a047c8f429
LEE YONGJIN <0jin.git@gmail.com>
2023-10-21 16:25:24 +0900

    Feat: 로그아웃 버튼 추가

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,29 +1,38 @@
-import Button from 'components/common/Button';
-import Input from 'components/common/Input';
+import React from 'react';
+// import Button from 'components/common/Button';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import './index.css'
-import CNU from './images/CNU.svg'
-// import KAIST from './images/KAIST_logo.png'
+import './index.css';
+import CNU from './images/CNU.svg';
+import Cookies from 'js-cookie';
 
-const Header = ({Uni}) => {
+const Header = ({ Uni }) => {
+  const handleLogout = () => {
+    // Cookies에서 이름이 "accessToken"인 토큰을 제거합니다.
+    Cookies.remove('accessToken');
+    // 로그아웃 후에 홈페이지를 다시 로드합니다.
+    window.location.href = '/login';
+  };
+
   return (
     <HeaderContainer>
       <SubContainer>
-      <h1 className="Nam" onClick={()=>{window.location.reload()}}>냠냠</h1>
-      <StyledLogo onClick={()=>{window.location.reload()}}>
-      <img src={CNU} width={'70px'} height={'70px'} alt='충남대'/>
-      {/* <img src={KAIST} width={'70px'} height={'70px'} alt='충남대'/> */}
-      </StyledLogo>
-
-      <Link to="/filter">
-        <Form>
-          <Input width='300px' placeholder='키워드 검색창으로 이동해요.' />
-          <Button type='button' width='72px'>
-            검색
-          </Button>
-        </Form>
-      </Link>
+        <h1 className="Nam" onClick={() => { window.location.reload() }}>냠냠</h1>
+        <StyledLogo onClick={() => { window.location.reload() }}>
+          <img src={CNU} width={'70px'} height={'70px'} alt='충남대' />
+        </StyledLogo>
+        
+          <Div>
+            <Link to="/filter">
+              <button type='button' width='150px' height='55px'>
+                키워드 검색
+              </button>
+            </Link>
+            <p onClick={handleLogout} type='button' width='72px'>
+              로그아웃
+            </p>
+          </Div>
+        
       </SubContainer>
     </HeaderContainer>
   );
@@ -41,7 +50,7 @@ const SubContainer = styled.div`
   align-items: center;
 `;
 
-const Form = styled.form`
+const Div = styled.div`
   display: flex;
   justify-content: space-between;
   width: 380px;
@@ -49,6 +58,6 @@ const Form = styled.form`
 
 const StyledLogo = styled.div`
   transform: translate(-350px,-5px);
-`
+`;
 
 export default Header;
